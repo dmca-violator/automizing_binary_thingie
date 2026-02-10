83ce558fa498e5a990d168b3e9d013c863d5332c
DMCA Violator <vahan.hovhannisyan09@gmail.com>
2026-02-10 21:38:04 +0400

    surrendering for now

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 me automizing my thinge lololo the binary thingie conversion or hexedeciaml or 8 base etc.
+
+10.2.2026 21:37
+Yeah I surrender for now I am too stupid for this
--- ze_code/automatic_conversion.cpp
+++ ze_code/automatic_conversion.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <cmath>
+
+using namespace std;
 
 int main(){
-    
+    double x, b;
+    cout << "Input the base to convert into: ";
+    cin >> b;
+    cout << "Input the X that will be converted into that base (input in base 10): ";
+    cin >> x;
+
+
+}
+
+double leftover_divisor(double x, double b){
+    string leftovers[0];
+    double t, got;
+    do{
+        got = x / b;
+        
+    }while
 }
